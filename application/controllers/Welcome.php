80422b2794e6e8d428a73fd829c6bda7a7305650
Cleber Maciel <macielcleberjr@gmail.com>
2017-11-07 21:25:27 -0200

    adicionado template para adm startmin bootstrap

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,6 +20,7 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('admin/template/header');
+		$this->load->view('admin/template/footer');
 	}
 }
