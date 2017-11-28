01daf4938661928a92502e3a67c49ed680c5390a
Cleber Maciel <macielcleberjr@gmail.com>
2017-11-28 09:38:37 -0200

    Adicionado base_url para site

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/arteni_ci/';
-
+//$config['base_url'] = 'http://localhost/arteni_ci/';
+$config['base_url'] = 'clebermaciel.online/arteni2/';
 /*
 |--------------------------------------------------------------------------
 | Index File
