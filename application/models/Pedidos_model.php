0203e0113625ca800bf066c296f9bda2562e86cc
root <macielcleberjr@gmail.com>
2018-04-08 23:14:33 -0300

    testes envio de notificacao para cliente

--- application/models/Pedidos_model.php
+++ application/models/Pedidos_model.php
@@ -33,7 +33,7 @@ class Pedidos_model extends CI_Model {
         $this->db->join('PEDIDOS', 'CLIENTE.ID_CLIENTE = PEDIDOS.CLIENTE');
         $this->db->where('PEDIDOS.ID_PEDIDOS', $referencia);
         $this->db->where('STATUS_VALIDO', 1);
-        return $this->db->get()->result();
+        return $this->db->get()->row();
     }
 
     function listar_tipo_materia($idi) {
